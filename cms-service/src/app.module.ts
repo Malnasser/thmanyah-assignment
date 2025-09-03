7aa5167ed277f4de2864c5edf4d2e9d2b89afbb7
mhalnasser <alnasser.cs@gmail.com>
2025-09-03 13:59:32 +0300

    chore: setup env config
    this setup for .env

--- cms-service/src/app.module.ts
+++ cms-service/src/app.module.ts
@@ -3,14 +3,34 @@ import { CmsModule } from './cms/cms.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppDataSource } from './data-source';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Program } from './cms/programs/entities/program.entity';
+import { Episode } from './cms/episodes/entities/episode.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     CmsModule,
     SharedModule,
     AuthModule,
-    TypeOrmModule.forRoot(AppDataSource.options),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Program, Episode],
+        migrations: [__dirname + '/../migrations/*.ts'],
+        synchronize: false,
+        logging: process.env.NODE_ENV == 'development',
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [],
   providers: [],
